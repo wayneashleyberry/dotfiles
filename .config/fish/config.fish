f11a516992bf2de6191432c67c95015c7dc0a6e2
wayneashleyberry <wayneashleyberry@gmail.com>
2019-08-12 13:28:23 +0100

    removes .config

--- .config/fish/config.fish
+++
@@ -1,4 +0,0 @@
-set -g fish_user_paths "/Users/wayne/go/bin" $fish_user_paths
-set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
-
-bass source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.bash.inc'
